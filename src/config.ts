7286e0750ec6e937f3dec50c2590ca33aecca6bd
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2024-02-09 16:52:17 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
+export {}; // Ensure file is treated as a module
+
 export const channelIDs = ["UCspaL6aZFjs5ChH0eaiakpw", "UC1bacrDsGPF_9LGfXPgEW-Q", "UCOghQW-IaXHoGphX0M3ZmWw"];
 export const chatIDs = [1885533743];
